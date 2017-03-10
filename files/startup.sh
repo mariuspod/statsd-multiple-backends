52b9e7eb6fbca643b186da394edce8ee44a5c439
Marius Podwyszynski <marius.pod@gmail.com>
2017-03-10 15:53:58 +0100

    Adding first version with a working elasticsearch-backend.

---
+++ files/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# start the uredir daemon
+/app/uredir -l debug -r $UREDIR_PORT && \
+
+# start the statsd with configured es-backend
+/app/statsd/bin/statsd /app/elasticsearch-config.js && \
+
+# wait for all subcommands
+wait
